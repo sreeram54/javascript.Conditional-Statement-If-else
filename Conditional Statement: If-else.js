5c8a397072b7abb695e4a75d216b31baac00cea9
sreeram54 <vaddisreeram54@gmail.com>
2022-08-17 02:05:46 +0530

    Conditional Statement: If-else

---
+++ Conditional Statement: If-else.js
@@ -0,0 +1,19 @@
+var mySal = 500;
+var yourSal = 1000;
+
+if( mySal > yourSal)
+{
+    alert("My Salary is greater than your salary");
+}
+else if(mySal < yourSal)
+{
+    alert("My Salary is less than your salary");
+}
+
+
+
+
+
+
+
+
